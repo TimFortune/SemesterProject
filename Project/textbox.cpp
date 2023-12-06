da26e1fd02cd141239fc661b1788246baae2522d
TimFortune <145366773+TimFortune@users.noreply.github.com>
2023-12-06 11:16:02 -0500

    Update textbox.cpp

--- Project/textbox.cpp
+++ Project/textbox.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define CLIENTID    "Beagle2"
 #define TOPIC       "TFortune/feeds/weather.blinks"
 #define AUTHMETHOD  "TFortune"
-#define AUTHTOKEN   "aio_zGNc943dtLcpW9cXfCKmhcXQVpyJ"
+#define AUTHTOKEN   ""
 #define QOS         1
 #define TIMEOUT     10000L
 #define LED_GPIO    "/sys/class/leds/beaglebone:green:usr2"
